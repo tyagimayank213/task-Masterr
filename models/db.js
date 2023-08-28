2e0bc95a3aad65bc35fe0bb3bde969a1910769bb
tyagimayank213 <76656921+tyagimayank213@users.noreply.github.com>
2023-08-28 20:20:26 +0530

    vercel deploy

--- models/db.js
+++ models/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 module.exports.init = async function(){
     await mongoose.connect(
-       "mongodb+srv://tyagimayank213:<password>@cluster0.r69kpve.mongodb.net/superCoderTodo?retryWrites=true&w=majority", {
+       "mongodb+srv://tyagimayank213:heavy3214@cluster0.r69kpve.mongodb.net/superCoderTodo?retryWrites=true&w=majority", {
         useNewUrlParser: true,
         useUnifiedTopology: true
       }
